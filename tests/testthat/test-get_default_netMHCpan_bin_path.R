c1e6627a42124a9b59d441ec730cd4ef5ffb47bd
tzina97 <g.tsaridou@gmail.com>
2021-07-22 14:17:42 +0300

    fixed tests

--- tests/testthat/test-get_default_netMHCpan_bin_path.R
+++ tests/testthat/test-get_default_netMHCpan_bin_path.R
@@ -3,6 +3,6 @@ context("test-get_default_netmhcpan_bin_path")
 test_that("use", {
   expect_equal(
     basename(get_default_netmhcpan_bin_path()),
-    "netmhcpan"
+    "netMHCpan"
   )
 })
--- tests/testthat/test-get_default_netMHCpan_subfolder.R
+++ tests/testthat/test-get_default_netMHCpan_subfolder.R
@@ -1,6 +1,6 @@
 test_that("use", {
   expect_equal(
     basename(get_default_netmhcpan_subfolder()),
-    paste0("NetMHCpan-", get_default_netmhcpan_version())
+    paste0("netMHCpan-", get_default_netmhcpan_version())
   )
 })
