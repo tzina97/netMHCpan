e09219ce7c5325095bad98bd5331baee0583bff3
richelbilderbeek <richel@richelbilderbeek.nl>
2021-07-23 11:52:02 +0200

    Use screamcase if netMHCpan is not installed

--- scripts/install_netmhcpan.sh
+++ scripts/install_netmhcpan.sh
@@ -12,8 +12,9 @@
 #
 cat R/*.R > tempscript.R
 echo "install_netmhcpan()" >> tempscript.R
-echo "if (!is_netmhcpan_installed()) return(314)" >> tempscript.R
 #echo "testthat::expect_true(is_netmhcpan_installed())" >> tempscript.R
+echo 'if (!is_netmhcpan_installed()) message("NETMHCPAN IS NOT INSTALLED!")' >> tempscript.R
+echo "if (!is_netmhcpan_installed()) return(314)" >> tempscript.R
 sed -i'.orginal' "s/netmhcpan:://g" tempscript.R
 cat tempscript.R
 Rscript tempscript.R
